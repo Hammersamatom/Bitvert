21125bb3f7d8186385938c9ea670590c7b60ea37
Joseph Samuel Pacheco-Corwin <joseph.corwin@baxter-academy.org>
2018-03-26 19:35:49 -0400

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <algorithm>
+#include <ctime>
+#include <cstdlib>
+
+using namespace std;
+
+auto invert(auto in)
+{
+    return ~in;
+}
+
+void main()
+{
+
+}
