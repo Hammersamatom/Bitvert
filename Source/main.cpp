a6851fccf9f3fc0e8f50851ec1f0e2c7a045b999
Joseph Samuel Pacheco-Corwin <joseph.corwin@baxter-academy.org>
2018-03-27 15:27:24 -0400

    meep
    gotta poo

--- Source/main.cpp
+++ Source/main.cpp
@@ -12,10 +12,10 @@ byte invert(byte in)
     return ~in;
 }
 
-int main()
+int main(int argc, char* argv[])
 {
-	//std::cin  >> temp;
-    //std::cout << temp         << ' ' << std::bitset<8>(temp).to_string()         << std::endl; 
-    std::cout << invert(temp) << ' ' << std::bitset<8>(invert(temp)).to_string() << std::endl; 
+	//std::cout << argv[1] << std::endl;
+    std::cout << argv[1]         << ' ' << std::bitset<8>(argv[1]).to_string()         << std::endl;
+    //std::cout << invert(argv[0]) << ' ' << std::bitset<8>(invert(argv[0])).to_string() << std::endl;
     return 0;
 }
