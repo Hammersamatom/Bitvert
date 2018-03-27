af995a0e155f3835f659914099b618c964b1e31f
Joseph Samuel Pacheco-Corwin <joseph.corwin@baxter-academy.org>
2018-03-27 12:52:13 -0400

    Some minor changes
    I was watching Life of Brian while working on this.

--- Source/main.cpp
+++ Source/main.cpp
@@ -5,7 +5,7 @@
 
 typedef unsigned char byte;
 
-byte temp = '1';
+byte temp = 'g';
 
 byte invert(byte in)
 {
@@ -14,7 +14,8 @@ byte invert(byte in)
 
 int main()
 {
-    std::cout << temp         << ' ' << std::bitset<8>(temp).to_string()         << std::endl; 
+	//std::cin  >> temp;
+    //std::cout << temp         << ' ' << std::bitset<8>(temp).to_string()         << std::endl; 
     std::cout << invert(temp) << ' ' << std::bitset<8>(invert(temp)).to_string() << std::endl; 
     return 0;
 }
