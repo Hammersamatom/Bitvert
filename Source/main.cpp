c17a0df2c454f8b4009423975ca22bf9ab82a7aa
Joseph Samuel Pacheco-Corwin <joseph.corwin@baxter-academy.org>
2018-03-27 21:23:06 -0400

    ARGH
    So many issues. Thankfully, god Hal is here.

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,21 +1,18 @@
 #include <iostream>
 #include <bitset>
 
-//using namespace std;
+using namespace std;
 
-typedef unsigned char byte;
+typedef  char byte;
 
-byte temp = 'g';
-
-byte invert(byte in)
+byte invert(byte* in)
 {
-    return ~in;
+	return ~*in;
 }
 
-int main(int argc, char* argv[])
+int main(int argc, byte *argv[])
 {
-	//std::cout << argv[1] << std::endl;
-    std::cout << argv[1]         << ' ' << std::bitset<8>(argv[1]).to_string()         << std::endl;
-    //std::cout << invert(argv[0]) << ' ' << std::bitset<8>(invert(argv[0])).to_string() << std::endl;
+    cout << argv[1] << endl;
+    cout << invert(argv[1]) << endl;
     return 0;
 }
