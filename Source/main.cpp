b94f19c4803c55491229a76159168e875b4dc518
Joseph Samuel Pacheco-Corwin <joseph.corwin@baxter-academy.org>
2018-03-28 18:24:21 -0400

    It works?
    Sort of? I have no idea why but it does.

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,18 +1,18 @@
 #include <iostream>
+#include <cstdlib> // for exit()
 #include <bitset>
 
 using namespace std;
 
-typedef  char byte;
-
-byte invert(byte* in)
+char invert(char in)
 {
-	return ~*in;
+	return ~in;
 }
 
-int main(int argc, byte *argv[])
+int main(int argc, char *argv[])
 {
-    cout << argv[1] << endl;
-    cout << invert(argv[1]) << endl;
+    cout << argv[1][0] << endl;
+    cout << invert(argv[1][0]) << endl;
+    cout << invert(invert(argv[1][0])) << endl;
     return 0;
 }
