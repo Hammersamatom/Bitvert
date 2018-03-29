c70526538707998adc071989f098c7c7c76fb7d1
Joseph Samuel Pacheco-Corwin <joseph.corwin@baxter-academy.org>
2018-03-29 18:58:44 -0400

    Some type of progress.
    I'm beginning to believe I'm insane.

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,18 +1,29 @@
 #include <iostream>
 #include <cstdlib> // for exit()
+#include <cstring>
 #include <bitset>
+#include <boost/dynamic_bitset.hpp>
 
 using namespace std;
 
-char invert(char in)
-{
-	return ~in;
-}
+const string temp = "";
 
 int main(int argc, char *argv[])
 {
-    cout << argv[1][0] << endl;
-    cout << invert(argv[1][0]) << endl;
-    cout << invert(invert(argv[1][0])) << endl;
+
+
+    if (argc == 1)
+        printf("Usage: %s \n", argv[0]);
+
+    const bitset<64> foo(string (argv[1]));
+    //const boost::dynamic_bitset<> foo(8, argv[1]);
+
+    cout << string(argv[1]) << endl;
+
+    cout << "ARGC: " << argc << "\n";
+    printf("Length o/ ARGV[1]: %lu \n", strlen(argv[1]));
+    printf("ARGV[1]: %s \n", argv[1]);
+    //cout << "ARGV[1] Bits: " << foo <<endl;
+
     return 0;
 }
